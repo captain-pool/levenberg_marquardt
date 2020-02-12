317c479913469869c7a6f55fdc800b77720e569f
captain-pool <rickdey1998@gmail.com>
2020-02-12 19:18:20 +0530

    Initial Commit

---
+++ sparse_tensor.py
@@ -0,0 +1,148 @@
+import torch_sparse
+import torch
+
+
+class SparseJacobian:
+    def __init__(
+        self, batch_size, shape, indices=None, values=None, sparse_jacobians=None
+    ):
+        self.batch_size = batch_size
+        self.indices = indices or SparseJacobian.map_on_batch(
+            lambda x: x.coalesce().indices(), sparse_jacobians, self.batch_size
+        )
+        self.values = values or SparseJacobian.map_on_batch(
+            lambda x: x.coalesce().values(), sparse_jacobians, self.batch_size
+        )
+        self.shape = shape
+        self.sparse_jacobians = sparse_jacobians or SparseJacobian.map_on_batch(
+            lambda arg: SparseJacobian._get_sparse_typeclass(arg[1])(arg[0], arg[1], self.shape),
+            list(zip(self.indices, self.values)),
+            self.batch_size,
+        )
+        self._transpose = None
+
+    @classmethod
+    def to_sparse_jacobian(cls, batched_jacobian):
+        batch_size = len(batched_jacobian)
+        sparse_jacobians = SparseJacobian.map_on_batch(SparseJacobian._to_sparse, batched_jacobian)
+        indices = SparseJacobian.map_on_batch(lambda x: x.coalesce().indices(), sparse_jacobians)
+        values = SparseJacobian.map_on_batch(lambda x: x.coalesce().values(), sparse_jacobians)
+        shape = sparse_jacobians[0].shape
+        return cls(batch_size, shape, indices, values, sparse_jacobians)
+    @property
+    def T(self):
+      if not self._transpose:
+        self._transpose = self.transpose()
+      return self._transpose
+    @staticmethod
+    def map_on_batch(function, batched_items, batch_size=None):
+        # Find a parallelized alternative
+        batch_size = batch_size or len(batched_items)
+        return [function(batched_items[index]) for index in range(batch_size)]
+    @staticmethod
+    def _get_sparse_typeclass(x):
+        x_typename = torch.typename(x).split(".")[-1]
+        sparse_tensortype = getattr(torch.sparse, x_typename)
+        return sparse_tensortype
+    @staticmethod
+    def _to_sparse(x):
+        """ converts dense tensor x to sparse format """
+        sparse_tensortype = SparseJacobian._get_sparse_typeclass(x)
+        indices = torch.nonzero(x)
+        if len(indices.shape) == 0:  # if all elements are zeros
+            return sparse_tensortype(*x.shape)
+        indices = indices.t()
+        values = x[tuple(indices[i] for i in range(indices.shape[0]))]
+        return sparse_tensortype(indices, values, x.size())
+
+    def matmul(self, other, keep_dense=False):
+        def sparse_sparse_matmul_fn(args):
+            return torch_sparse.spspmm(
+                *args, m=self.shape[0], k=self.shape[1], n=other.shape[1]
+            )
+
+        def sparse_dense_matmul_fn(args):
+            return torch.sparse.mm(*args)
+
+        if isinstance(other, SparseJacobian):
+            interim_items = SparseJacobian.map_on_batch(
+                sparse_sparse_matmul_fn,
+                list(zip(self.indices, self.values, other.indices, other.values)),
+                self.batch_size,
+            )
+            interim_indices, interim_values = list(zip(*interim_items))
+            return SparseJacobian(
+                self.batch_size,
+                torch.Size([self.shape[0], other.shape[1]]),
+                interim_indices,
+                interim_values,
+            )
+        dense_products = SparseJacobian.map_on_batch(
+            sparse_dense_matmul_fn,
+            list(zip(self.sparse_jacobians, other)),
+            self.batch_size,
+        )
+        if keep_dense:
+            return torch.stack(dense_products)
+        return SparseJacobian.to_sparse_jacobian(dense_products)
+
+    def transpose(self):
+        def transpose_fn(sparse_jacobian):
+            return sparse_jacobian.transpose(0, 1)
+
+        sparse_jacobians = SparseJacobian.map_on_batch(
+            transpose_fn, self.sparse_jacobians, self.batch_size
+        )
+        return SparseJacobian(
+            self.batch_size,
+            torch.Size([self.shape[1], self.shape[0]]),
+            sparse_jacobians=sparse_jacobians,
+        )
+
+    def __add__(self, other):
+        def add_fn(sparses):
+            return sparses[0] + sparses[1]
+
+        if isinstance(other, SparseJacobian):
+            added_jacobians = SparseJacobian.map_on_batch(
+                add_fn,
+                list(zip(self.sparse_jacobians, other.sparse_jacobians)),
+                self.batch_size,
+            )
+        else:
+            added_jacobians = SparseJacobian.map_on_batch(
+                lambda x: other + x, self.sparse_jacobians, self.batch_size
+            )
+
+        return SparseJacobian(
+            self.batch_size, self.shape, sparse_jacobians=added_jacobians
+        )
+
+    def __mul__(self, other):
+        if isinstance(other, SparseJacobian):
+            raise NotImplementedError
+        else:
+            multiplied_jacobians = SparseJacobian.map_on_batch(
+                lambda x: x * other, self.sparse_jacobians, self.batch_size
+            )
+        return SparseJacobian(
+            self.batch_size, self.shape, sparse_jacobians=multiplied_jacobians
+        )
+    
+    def inverse(self):
+      def inverse_fn(sparse):
+        # This is a really naive approach. Look for a better alternative.
+        # This is the only solution as of now. Plus there's another reason
+        # Inverse of a sparse matrix might not be a sparse matrix
+        return torch.inverse(sparse.to_dense())
+      if self.shape[0] != self.shape[1]:
+        raise ValueError(f"Cannot invert matrix of shape: [{self.shape[0]}, {self.shape[1]}]. Need a square matrix")
+      denses = SparseJacobian.map_on_batch(inverse_fn, self.sparse_jacobians, self.batch_size)
+      return SparseJacobian.to_sparse_jacobian(denses)
+
+    def to_dense(self):
+        def to_dense_fn(sparse):
+            return sparse.to_dense()
+
+        denses = SparseJacobian.map_on_batch(to_dense_fn, self.sparse_jacobians, self.batch_size)
+        return torch.stack(denses)
---
+++ test_lm.py
@@ -0,0 +1,73 @@
+import torch
+import sparse_tensor as st
+
+def F(X, theta):
+  return X @ (theta**2)
+
+def residual(theta, Y, X):
+  return Y - F(X, theta) # X.shape = [3, 1], Y.shape = [3, 1]
+
+X = torch.rand(1, 10).normal_()
+theta = torch.rand(10, 5).normal_().requires_grad_(True)
+Y = torch.rand(1, 5).normal_()
+
+class LMOptimizer(object):
+  def __init__(self, batch_size, parameters, damping=1):
+    self._batch_size = batch_size
+    self._parameters = parameters
+    self._flattened_parameters = self._flatten(self._parameters)
+    self._damping = damping
+  
+  def _numel(self, tensor):
+    return abs(tensor.numel() // self._batch_size)
+
+  def _flatten(self, tensor):
+    numel = self._numel(tensor)
+    return tensor.reshape([self._batch_size, numel])
+
+  def zero_grad(self):
+    for param in self._flattened_parameters:
+      if param.grad:
+        param.grad.detach_()
+        param.grad.zero_()
+    for param in self._parameters:
+      if param.grad:
+        param.grad.detach_()
+        param.grad.zero_()
+
+  def _get_jacobian(self, y, create_graph=False):
+    jac = []
+    flat_y = y.reshape(-1)
+    x = self._parameters
+    grad_y = torch.zeros_like(flat_y)
+    for i in range(len(flat_y)):
+      grad_y[i] = 1.
+      grad_x,  = torch.autograd.grad(flat_y, x, grad_y, retain_graph=True, create_graph=create_graph)
+      jac.append(grad_x.reshape(x.shape))
+      grad_y[i] = 0.
+    return torch.stack(jac).reshape([-1, self._numel(y), self._numel(x)])
+
+  def _pseudo_inverse(self, jacobian):
+    jacobian = st.SparseJacobian.to_sparse_jacobian(jacobian)
+    jacobian_abs = jacobian.T.matmul(jacobian)
+
+    marquardt = torch.eye(*jacobian_abs.shape).repeat(jacobian_abs.batch_size, 1)
+    if len(marquardt.shape) != 3:
+      marquardt = marquardt.unsqueeze(0)
+    marquardt = st.SparseJacobian.to_sparse_jacobian(marquardt)
+    return (jacobian_abs + marquardt * self._damping).inverse().matmul(jacobian.T)
+
+  def step(self, output_tensor):
+    jacobian = self._get_jacobian(output_tensor)
+    ps_inv = self._pseudo_inverse(jacobian)
+    self._flattened_parameters.data -= torch.squeeze(ps_inv.matmul(
+                                                                  torch.unsqueeze(
+                                                                    output_tensor, -1), keep_dense=True), -1)
+o = residual(theta, Y, X)
+opt = LMOptimizer(o.shape[0], theta, damping=0.13)
+
+while True:
+  o = residual(theta, Y, X)
+  print(o.mean().data.numpy())
+  opt.zero_grad()
+  opt.step(o)
